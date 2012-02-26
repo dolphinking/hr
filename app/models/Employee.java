53d4f5ed5b646b4d54baafbb8e0178d45ea44bc1
Suraj Tamang <surajyonzon@gmail.com>
2012-02-26 16:11:53 +0545

    Added three model Department, Role and Employee, added Department Controller and used MySQL Database as default database and written some test regrading the Employee, Role and Department

---
+++ app/models/Employee.java
@@ -0,0 +1,47 @@
+package models;
+
+import play.*;
+import play.db.jpa.*;
+import play.data.validation.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Employee extends Model {
+	
+	@Required
+	public String fullName;
+	
+	@Required
+	public String userName;
+	
+	@Required
+	public String password;
+	
+	@Required
+	public String email;
+	
+	@Required
+	public String phone;
+	
+	@ManyToOne
+	@Required
+	public Department department;
+	
+	@ManyToOne
+	@Required
+	public Role role;
+	
+	public Employee(String fullName, String userName, String password, String email, String phone, Department department, Role role) {
+		this.fullName = fullName;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+		this.department = department;
+		this.role = role;
+	}
+	
+}
+
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -4,10 +4,63 @@ import play.test.*;
 import models.*;
 
 public class BasicTest extends UnitTest {
+	
+	/*
+	* Test for creating and retrieving Department from the database.
+	*/
+	@Test
+	public void createAndRetrieveDepartment() {
+		// Created new instance of department to save the sample data.
+		new Department("Accounts").save();
+		
+		// Created the instance of department and searched for first data.
+		Department department = Department.find("byName", "Accounts").first();
+		
+		//Test
+		assertNotNull(department);
+		assertEquals("Accounts", department.name);
+	}
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+	/*
+	* Test for creating and retrieving Role from the database.
+	*/
+	@Test
+	public void createAndRetrieveRole() {
+		// Created new instance of department to save the sample data.
+		new Role("Admin").save();
+		
+		// Created the instance of department and searched for first data.
+		Role role = Role.find("byName", "Admin").first();
+		
+		//Test
+		assertNotNull(role);
+		assertEquals("Admin", role.name);
+	}
+	
+	
+	/*
+	* Test for creating and retrieving Employee from the database.
+	*/
+	@Test
+	public void createAndRetrieveEmployee() {
+		// Create the department table at first.
+		Department department = new Department("Accounts").save();
+		
+		// Create the role table at first.
+		Role role = new Role("Admin").save();
+		
+		// Created new instance of employee to save the sample data.
+		new Employee("Suraj Tamang", "suraj", "suraj123", "surajyonzon@gmail.com", "9849038521", department, role).save();
+		
+		// Created the instance of Employee and searched for first data.
+		Employee employee = Employee.find("byUserName", "suraj").first();
+		
+		//Test
+		assertNotNull(employee);
+		assertEquals("suraj", employee.userName);
+	}
+	
+	
+	
 
 }
