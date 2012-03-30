be2401fa14e92591ceab2a7f6cebe54f6f3dd6e9
Suraj Tamang <surajyonzon@gmail.com>
2012-03-30 10:14:17 +0545

    Added captcha feature to the application

--- app/controllers/Applicants.java
+++ app/controllers/Applicants.java
@@ -2,6 +2,9 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.libs.*;
+import play.cache.*;
+import play.data.validation.*;
 
 import java.util.*;
 
@@ -10,7 +13,8 @@ import models.*;
 public class Applicants extends Controller {
 
 	public static void signup() {
-		render();
+		String randomID = Codec.UUID();
+		render(randomID);
 	}
 	
 	public static void login() {
@@ -24,6 +28,13 @@ public class Applicants extends Controller {
 	public static void save() {
 		render();
 	}
+	
+	public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#EEE");
+        Cache.set(id, code, "30mn");
+        renderBinary(captcha);
+    }
 
 }
 
