238e65e5a44bad5b13567d1e119581d2865ef024
Suraj Tamang <surajyonzon@gmail.com>
2012-04-26 11:20:43 +0545

    Added the new Applicant save feature and added new Mailer-Mails for sending information to the client

---
+++ app/notifiers/Mails.java
@@ -0,0 +1,25 @@
+package notifiers;
+ 
+import play.*;
+import models.*;
+import play.mvc.*;
+import java.util.*;
+ 
+public class Mails extends Mailer {
+
+	public static void welcome(Applicant applicant) {
+		setSubject("Welcome %s", applicant.firstName);
+		addRecipient(applicant.email);
+		setFrom("HumanEquationUAE <humanequationtest@gmail.com>");
+		send(applicant);
+	}
+
+	public static void lostPassword(Applicant applicant) {
+		String newpassword = applicant.password;
+		setFrom("HumanEquationUAE <humanequationtest@gmail.com>");
+		setSubject("Your password has been reset");
+		addRecipient(applicant.email);
+		send(applicant, newpassword);
+	}
+
+}
