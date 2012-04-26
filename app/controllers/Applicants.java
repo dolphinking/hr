d491ef7bd97140337663d7a8e0c9c360f6b56183
Suraj Tamang <surajyonzon@gmail.com>
2012-04-26 20:08:00 +0545

    Added new profile page for applicant

--- app/controllers/Applicants.java
+++ app/controllers/Applicants.java
@@ -12,22 +12,70 @@ import models.*;
 import notifiers.*;
 
 public class Applicants extends Controller {
+	
+	@Before
+  static void addApplicant() {
+		Applicant applicant = connectApplicant();
+    if(applicant != null) {
+			renderArgs.put("userApplicant", applicant);
+		}
+  }
+  
+  static Applicant connectApplicant() {
+    if(renderArgs.get("userApplicant") != null) {
+			return renderArgs.get("userApplicant", Applicant.class);
+		}
+		String email = session.get("userApplicant");
+		if(email != null) {
+			return Applicant.find("byEmail", email).first();
+		}
+		return null;
+  }
 
-	public static void signup() {
-		String randomID = Codec.UUID();
-		render(randomID);
+	// Checks Session...
+  static void checkApplicantSession() {
+		if(connectApplicant() == null) {
+			flash.error("Please log in first.");
+			login();
+		}
+  }
+	
+	// @username is the username variable to capture the value of username
+	// @password is the password variable to capture the value of password
+	public static void checkApplicant(String email, String password) {
+		Applicant applicant = Applicant.find("byEmailAndPassword", email, password).first();
+		if(applicant != null) {
+			session.put("userApplicant", applicant.email);
+			index();
+		}
+		// if the username or password is invalid....
+		flash.put("email", email);
+		flash.error("Invalid username or password...");
+		login();
 	}
 	
 	public static void login() {
 		render();
 	}
 	
-	public static void checkUser() {
-		
+	public static void index() {
+		checkApplicantSession();
+		Applicant applicant = connectApplicant();
+		render(applicant);
+	}
+	
+	public static void logout() {
+		session.clear();
+		login();
+	}
+	
+	public static void signup() {
+		String randomID = Codec.UUID();
+		render(randomID);
 	}
     
-	public static void save(@Valid Applicant applicant, String verifyPassword,
-		String code, String randomID) {
+	public static void save(@Valid Applicant applicant, 
+		String verifyPassword, String code, String randomID) {
 		
 		validation.required(applicant.firstName);
 		validation.required(applicant.lastName);
@@ -58,4 +106,3 @@ public class Applicants extends Controller {
 	}
 
 }
-
